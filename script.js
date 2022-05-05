067385badf974de2f05b1108f15b56f941de2932
James ORourke <jimao137@gmail.com>
2022-05-04 23:09:44 -0400

    debug

--- script.js
+++ script.js
@@ -26,26 +26,25 @@ function playRound(playerSelection, computerSelection) {
         }
 
     } else if (playerInput == "paper") {
-        if (computerSelection == "paper") {
-            return "You lose! Paper covers Rock!";
+        if (computerSelection == "rock") {
+            return "You win! Paper covers Rock!";
         } else {
-            return "You win! Rock beats Scissors!";
+            return "You lose! Scissors cuts Paper!";
         }
 
     } else if (playerInput == "scissors") {
         if (computerSelection == "paper") {
-            return "You lose! Paper covers Rock!";
+            return "You win! Scissors cut Paper!";
         } else {
-            return "You win! Rock beats Scissors!";
+            return "You lose! Rock beats Scissors!";
         }
-        
+
     } else {
         console.log("Misspelled. Try again");
-        playRound(prompt("Misspelled. Try again. choose your weapon"), computerSelection);
+        console.log(playRound(prompt("Misspelled. Try again. choose your weapon"), computerChoice));
     }
-    console.log(`You ${outcome}!`)
 }
 
-    const playerSelection = prompt("choose your weapon");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+    const playerChoice = prompt("choose your weapon");
+    const computerChoice = computerPlay();
+    console.log(playRound(playerChoice, computerChoice));
